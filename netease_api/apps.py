64c3166f4ecfc9fb30930c3244dac4a586caa09f
Vcup <vcupmoe@gmail.com>
2020-06-22 19:25:22 +0800

    添加应用 netease_api

---
+++ netease_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NeteaseApiConfig(AppConfig):
+    name = 'netease_api'
---
+++ netease_api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'netease_api'
+urlpatterns = []
