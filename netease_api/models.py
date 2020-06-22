1743dd41e6e1baaea1fc61f42863a465c39da7eb
Vcup <vcupmoe@gmail.com>
2020-06-23 00:35:11 +0800

    为所有模型的 models.IntegerField 删除 max_length=xxx 的参数 给还没有添加 __str__ 魔法的模型添加返回对应 name 的魔法

--- netease_api/models.py
+++ netease_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Song(models.Model):
     name = models.CharField(max_length=50, verbose_name='Song name')
-    sid = models.IntegerField(max_length=16, verbose_name='Song id')
+    sid = models.IntegerField(verbose_name='Song id')
 
     def __str__(self):
         return self.name
@@ -12,25 +12,37 @@ class Song(models.Model):
 class Album(models.Model):
     song = models.ManyToManyField('Song', verbose_name='Album songs')
     name = models.CharField(max_length=50, verbose_name='Album name')
-    aid = models.IntegerField(max_length=16, verbose_name='Album id')
+    aid = models.IntegerField(verbose_name='Album id')
+
+    def __str__(self):
+        return self.name
 
 
 class Artist(models.Model):
     song = models.ManyToManyField('Song', verbose_name='Artist songs')
     album = models.ManyToManyField('Album', verbose_name='Artist albums')
     name = models.CharField(max_length=30, verbose_name='Artist name')
-    aid = models.IntegerField(max_length=16, verbose_name='Artist id')
+    aid = models.IntegerField(verbose_name='Artist id')
+
+    def __str__(self):
+        return self.name
 
 
 class PlayList(models.Model):
     song = models.ManyToManyField('Song', verbose_name='Playlist songs')
     name = models.CharField(max_length=40, verbose_name='Playlist name')
-    pid = models.IntegerField(max_length=16, verbose_name='Playlist id')
-    type = models.IntegerField(max_length=1, verbose_name='Playlist type')
+    pid = models.IntegerField(verbose_name='Playlist id')
+    type = models.IntegerField(verbose_name='Playlist type')
+
+    def __str__(self):
+        return self.name
 
 
 class User(models.Model):
     song = models.ManyToManyField('Song', verbose_name='User songs')
     playlist = models.ManyToManyField('PlayList', verbose_name='User playlists')
     name = models.CharField(max_length=30, verbose_name='User name')
-    uid = models.IntegerField(max_length=16, verbose_name='User id')
+    uid = models.IntegerField(verbose_name='User id')
+
+    def __str__(self):
+        return self.name
