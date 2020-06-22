4836465982ea9e406bd8c5df9a5bc291fa702e33
Vcup <vcupmoe@gmail.com>
2020-06-23 00:28:13 +0800

    在first_site/settings.py 中添加了应用netease_api；添加了五个模型；为应用netease迁移一次

--- first_site/settings.py
+++ first_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'netease_api.apps.NeteaseApiConfig'
 ]
 
 MIDDLEWARE = [
--- netease_api/models.py
+++ netease_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Song name')
+    sid = models.IntegerField(max_length=16, verbose_name='Song id')
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    song = models.ManyToManyField('Song', verbose_name='Album songs')
+    name = models.CharField(max_length=50, verbose_name='Album name')
+    aid = models.IntegerField(max_length=16, verbose_name='Album id')
+
+
+class Artist(models.Model):
+    song = models.ManyToManyField('Song', verbose_name='Artist songs')
+    album = models.ManyToManyField('Album', verbose_name='Artist albums')
+    name = models.CharField(max_length=30, verbose_name='Artist name')
+    aid = models.IntegerField(max_length=16, verbose_name='Artist id')
+
+
+class PlayList(models.Model):
+    song = models.ManyToManyField('Song', verbose_name='Playlist songs')
+    name = models.CharField(max_length=40, verbose_name='Playlist name')
+    pid = models.IntegerField(max_length=16, verbose_name='Playlist id')
+    type = models.IntegerField(max_length=1, verbose_name='Playlist type')
+
+
+class User(models.Model):
+    song = models.ManyToManyField('Song', verbose_name='User songs')
+    playlist = models.ManyToManyField('PlayList', verbose_name='User playlists')
+    name = models.CharField(max_length=30, verbose_name='User name')
+    uid = models.IntegerField(max_length=16, verbose_name='User id')
