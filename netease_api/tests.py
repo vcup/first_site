f85d803352b10aad0fc23fc10e68a6cfbb79d8fd
Vcup <vcupmoe@gmail.com>
2020-06-24 14:06:04 +0800

    更新测试实例，同时减少不必要代码

--- netease_api/tests.py
+++ netease_api/tests.py
@@ -8,30 +8,27 @@ class ModelTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.s1 = Song(name='s1', sid=10)
-        cls.s2 = Song(name='s2', sid=20)
-        cls.s3 = Song(name='s3', sid=30)
+        cls.s1 = Song.objects.create(name='s1', id=10)
+        cls.s2 = Song.objects.create(name='s2', id=20)
+        cls.s3 = Song.objects.create(name='s3', id=30)
         cls.s1.save(), cls.s2.save(), cls.s3.save()
 
-        cls.al1 = Album(name='al1', aid=11)
-        cls.al2 = Album(name='al2', aid=21)
-        cls.al3 = Album(name='al3', aid=31)
+        cls.al1 = Album.objects.create(name='al1', id=11)
+        cls.al2 = Album.objects.create(name='al2', id=21)
+        cls.al3 = Album.objects.create(name='al3', id=31)
         cls.al1.save(), cls.al2.save(), cls.al3.save()
 
-        cls.a1 = Artist(name='a1', aid=12)
-        cls.a2 = Artist(name='a2', aid=22)
-        cls.a3 = Artist(name='a3', aid=32)
-        cls.a1.save(), cls.a2.save(), cls.a3.save()
+        cls.a1 = Artist.objects.create(name='a1', id=12)
+        cls.a2 = Artist.objects.create(name='a2', id=22)
+        cls.a3 = Artist.objects.create(name='a3', id=32)
 
-        cls.u1 = User(name='u1', uid=14)
-        cls.u2 = User(name='u2', uid=24)
-        cls.u3 = User(name='u3', uid=34)
-        cls.u1.save(), cls.u2.save(), cls.u3.save()
+        cls.u1 = User.objects.create(name='u1', id=14)
+        cls.u2 = User.objects.create(name='u2', id=24)
+        cls.u3 = User.objects.create(name='u3', id=34)
 
-        cls.p1 = PlayList(name='p1', pid=13, createTime=timezone.now(), master_user=cls.u3)
-        cls.p2 = PlayList(name='p2', pid=23, type=5, createTime=timezone.now(), master_user=cls.u3)
-        cls.p3 = PlayList(name='p3', pid=33, type=1, createTime=timezone.now(), master_user=cls.u3)
-        cls.p1.save(), cls.p2.save(), cls.p3.save()
+        cls.p1 = PlayList.objects.create(name='p1', id=13, createTime=timezone.now(), master_uid=34)
+        cls.p2 = PlayList.objects.create(name='p2', id=23, type=5, createTime=timezone.now(), master_uid=34)
+        cls.p3 = PlayList.objects.create(name='p3', id=33, type=1, createTime=timezone.now(), master_uid=34)
 
         cls.a1.album.add(cls.al1)
         cls.a2.album.add(cls.al1, cls.al2)
@@ -60,7 +57,7 @@ class SongModelTests(ModelTests):
         self.assertEqual((self.s1.name, self.s2.name, self.s3.name), ('s1', 's2', 's3'))
 
     def test_song_id(self):
-        self.assertEqual((self.s1.sid, self.s2.sid, self.s3.sid), (10, 20, 30))
+        self.assertEqual((self.s1.id, self.s2.id, self.s3.id), (10, 20, 30))
 
     def test_s1_in_album(self):
         self.assertEqual(list(self.s3.album_set.all()), [self.al3])
@@ -88,7 +85,7 @@ class AlbumModelTests(ModelTests):
         self.assertEqual((self.al1.name, self.al2.name, self.al3.name), ('al1', 'al2', 'al3'))
 
     def test_album_id(self):
-        self.assertEqual((self.al1.aid, self.al2.aid, self.al3.aid), (11, 21, 31))
+        self.assertEqual((self.al1.id, self.al2.id, self.al3.id), (11, 21, 31))
 
     def test_al1_in_artist(self):
         self.assertEqual(list(self.al3.artist_set.all()), [self.a3])
@@ -106,7 +103,7 @@ class ArtistModelTests(ModelTests):
         self.assertEqual((self.a1.name, self.a2.name, self.a3.name), ('a1', 'a2', 'a3'))
 
     def test_artist_id(self):
-        self.assertEqual((self.a1.aid, self.a2.aid, self.a3.aid), (12, 22, 32))
+        self.assertEqual((self.a1.id, self.a2.id, self.a3.id), (12, 22, 32))
 
     def test_a1_have_song(self):
         self.assertEqual(self.a1.return_self_have_song(), [[self.s1]])
@@ -124,7 +121,7 @@ class PlayListModelTests(ModelTests):
         self.assertEqual((self.p1.name, self.p2.name, self.p3.name), ('p1', 'p2', 'p3'))
 
     def test_playlist_id(self):
-        self.assertEqual((self.p1.pid, self.p2.pid, self.p3.pid), (13, 23, 33))
+        self.assertEqual((self.p1.id, self.p2.id, self.p3.id), (13, 23, 33))
 
     def test_playlist_type(self):
         self.assertEqual((self.p1.type, self.p2.type, self.p3.type), (0, 5, 1))
@@ -136,7 +133,7 @@ class UserModelTests(ModelTests):
         self.assertEqual((self.u1.name, self.u2.name, self.u3.name), ('u1', 'u2', 'u3'))
 
     def test_user_id(self):
-        self.assertEqual((self.u1.uid, self.u2.uid, self.u3.uid), (14, 24, 34))
+        self.assertEqual((self.u1.id, self.u2.id, self.u3.id), (14, 24, 34))
 
     def test_u1_have_song(self):
         self.assertEqual(self.u1.return_self_have_song(), [[self.s1]])
