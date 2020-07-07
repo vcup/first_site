a416452d7192f78a15f5b323f5f2f45d29f6e0ff
vcup <vcupmoe@gmail.com>
2020-07-07 17:57:42 +0800

    添加管理账户，为所有模型注册管理界面

--- netease_api/admin.py
+++ netease_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from netease_api.models import User, PlayList, Artist, Album, Song, Tag, Alias
+
+admin.site.register(User)
+admin.site.register(PlayList)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Tag)
+admin.site.register(Alias)
